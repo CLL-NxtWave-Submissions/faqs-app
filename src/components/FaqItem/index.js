c2fd9de20c958ce8e4b281740610906f8499f8f8
Surya T N <suryatej.nim@yahoo.com>
2022-09-20 15:49:00 +0000

    Implemented FaqItem functional component to render intended UI for single faq list item

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,39 @@
-// Write your code here.
+import './index.css'
+
+const FaqItem = props => {
+  const {itemData, answerDisplayToggleHandler, showAnswer} = props
+  const {id, questionText, answerText} = itemData
+
+  const onAnswerDisplayToggleButtonClick = () => answerDisplayToggleHandler(id)
+
+  return (
+    <li className="faq-item-container">
+      <div className="faq-question-container">
+        <p className="faq-question-text">{questionText}</p>
+        <button
+          type="button"
+          className="faq-toggle-answer-button"
+          onClick={onAnswerDisplayToggleButtonClick}
+        >
+          <img
+            className="faq-toggle-answer-button-img"
+            src={
+              showAnswer
+                ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+                : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png '
+            }
+            alt={showAnswer ? 'minus' : 'plus'}
+          />
+        </button>
+      </div>
+      {showAnswer && (
+        <>
+          <hr className="faq-question-answer-separator" />
+          <p className="faq-answer-text">{answerText}</p>
+        </>
+      )}
+    </li>
+  )
+}
+
+export default FaqItem
