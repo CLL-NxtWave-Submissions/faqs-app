ab42c3198996b9b07cdad8c50e13d22f7cdc20ea
Surya T N <suryatej.nim@yahoo.com>
2022-09-20 15:30:31 +0000

    Added CSS styling of the unorldered list of faqs in index.css. Added bare-bones implementation of the Faqs class component, until the state intialization and rendering JSX elements until the unordered list of faqs, without any faqs

--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,31 @@
-// Write your code here.
+import {Component} from 'react'
+import './index.css'
+
+export default class Faqs extends Component {
+  state = {
+    faqsToggleAnswerStatusList: this.generateInitialFaqsToggleAnswerStatusList(),
+  }
+
+  generateInitialFaqsToggleAnswerStatusList = () => {
+    const {faqsList} = this.props
+
+    return faqsList.map(faqsListItem => ({
+      faqId: faqsListItem.id,
+      showAnswer: false,
+    }))
+  }
+
+  render() {
+    const {faqsToggleAnswerStatusList} = this.state
+    const {faqsList} = this.props
+
+    return (
+      <div className="faqs-bg-container">
+        <div className="faqs-content-container">
+          <h1 className="faqs-header">FAQs</h1>
+          <ul className="faqs-list"></ul>
+        </div>
+      </div>
+    )
+  }
+}
